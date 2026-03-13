5273e0585588ca246c5157a9f7341072bdbc1974
miismegtiis <hegyi.kamilla.bh@gmail.com>
2026-03-13 15:10:59 +0100

    Helyrehoztam_Update test_mymath.py

--- test_mymath.py
+++ test_mymath.py
@@ -3,16 +3,21 @@ import pytest
 from mymath import add_numbers
 
 def test_add_positive():
-    assert add_numbers(1, 2) == 4
+    assert add_numbers(1, 2) == 3
+
 
 def test_add_zero():
     assert add_numbers(1, 0) == 1
 
+
 def test_add_negative():
     assert add_numbers(4, -100) == -96
+
 
 def test_add_string__expect_exception():
     with pytest.raises(TypeError):
         add_numbers(4, 'I DO NOT BELONG HERE')
+        
+
 
 
